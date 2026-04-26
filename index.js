ab8d228e245e48086e64a50e5c7e01dceb7e3113
rafsan1p <rubayetrafsan605@gmail.com>
2026-04-27 01:50:52 +0600

    post parcel data and create parcel get api

--- index.js
+++ index.js
@@ -33,7 +33,15 @@ async function run() {
 
     //parcel api
     app.get('/parcels', async (req, res) => {
+        const query = {}
+        const {email} = req.query;
+        if(email){
+            query.senderEmail = email;
+        }
 
+        const cursor = parcelsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
     })
     app.post('/parcels', async(req, res) => {
         const parcel = req.body;
